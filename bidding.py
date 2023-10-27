943a8e0101c367b9d45aa2b9b4ceeb431f3a799e
bonane <ndayishimyebonane@gmail.com>
2023-10-27 23:35:03 +0200

    bidding

--- bidding.py
+++ bidding.py
@@ -1,13 +1,43 @@
-def format_name(f_name, l_name):
-    """Take a first and last name qnd format it to return the title case version of the name."""
-    
-    formated_f_name = f_name.title()
-    formated_l_name = l_name.title()
-  
-    return f"{formated_f_name} {formated_l_name}"
+#Calculator
+
+def add(n1, n2):
+    return n1 + n2
 
 
-formated_string =  format_name('BONANE', 'ndayishimye')
-print(formated_string)
+def subtract(n1, n2):
+    return n1 - n2
 
 
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+
+
+def calculator():
+
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    quit_calc = False
+
+    while not quit_calc:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        num1 = answer
+        decision = input(
+            f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: "
+        ).lower()
+        if decision == 'n':
+            quit_calc = True
+
+
+calculator()
